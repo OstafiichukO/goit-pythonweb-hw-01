aaf894fd3e60f871853e29c2cfe2b9e0aa14e596
ostafiichuko <issimo333@gmail.com>
2025-03-05 13:24:13 +0200

    feat: task 1, factoty pattern

--- factory_pattern.py
+++ factory_pattern.py
@@ -1,17 +1,58 @@
-# This is a sample Python script.
+class Vehicle:
+    def __init__(self, make: str, model: str, region_spec: str):
+        self.make = make
+        self.model = model
+        self.region_spec = region_spec
 
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-
-    print(f"Hi, {name}")  # Press F9 to toggle the breakpoint.
+    def start_engine(self):
+        pass
 
 
-# Press the green button in the gutter to run the script.
+class Car(Vehicle):
+    def start_engine(self):
+        print(f"{self.make} {self.model} {self.region_spec}: Двигун запущено")
+
+
+class Motorcycle(Vehicle):
+    def start_engine(self):
+        print(f"{self.make} {self.model} {self.region_spec}: Мотор заведено")
+
+
+class VehicleFactory:
+    def create_car(self, make: str, model: str) -> Vehicle:
+        pass
+
+    def create_motorcycle(self, make: str, model: str) -> Vehicle:
+        pass
+
+
+class USVehicleFactory(VehicleFactory):
+    def create_car(self, make: str, model: str) -> Vehicle:
+        return Car(make, model, "US")
+
+    def create_motorcycle(self, make: str, model: str) -> Vehicle:
+        return Motorcycle(make, model, "US")
+
+
+class EUVehicleFactory(VehicleFactory):
+    def create_car(self, make: str, model: str) -> Vehicle:
+        return Car(make, model, "EU")
+
+    def create_motorcycle(self, make: str, model: str) -> Vehicle:
+        return Motorcycle(make, model, "EU")
+
+
 if __name__ == "__main__":
-    print_hi("PyCharm")
+    us_factory = USVehicleFactory()
+    us_car = us_factory.create_car("Tesla", "model Y")
+    us_motorcycle = us_factory.create_motorcycle("Halcyon", "250")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    us_car.start_engine()
+    us_motorcycle.start_engine()
+
+    eu_factory = EUVehicleFactory()
+    eu_car = eu_factory.create_car("Volkswagen", "Tiguan")
+    eu_motorcycle = eu_factory.create_motorcycle("Yamaha", "150")
+
+    eu_car.start_engine()
+    eu_motorcycle.start_engine()
